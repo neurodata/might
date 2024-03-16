c635bbd3727571cb629ab3c9542bae68bbc77326
Adam Li <adam2392@gmail.com>
2024-03-16 09:42:55 -0400

    Run more multi-equal
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- exps/new_submission/Figure6_comight_vs_nsamples_ndims/comight_sa98_vs_ndims.py
+++ exps/new_submission/Figure6_comight_vs_nsamples_ndims/comight_sa98_vs_ndims.py
@@ -494,7 +494,7 @@ MODEL_NAMES = {
     "might": {
         "n_estimators": n_estimators,
         "honest_fraction": 0.5,
-        "n_jobs": 10,
+        "n_jobs": 1,
         "bootstrap": True,
         "stratify": True,
         "max_samples": 1.6,
