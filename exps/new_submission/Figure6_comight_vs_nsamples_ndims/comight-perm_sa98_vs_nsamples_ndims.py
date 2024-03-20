39186cc5030e8559a6bbbe510fa7ece088679645
Adam Li <adam2392@gmail.com>
2024-03-20 17:17:25 -0400

    Fix comight perm
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- exps/new_submission/Figure6_comight_vs_nsamples_ndims/comight-perm_sa98_vs_nsamples_ndims.py
+++ exps/new_submission/Figure6_comight_vs_nsamples_ndims/comight-perm_sa98_vs_nsamples_ndims.py
@@ -244,7 +244,7 @@ if __name__ == "__main__":
 
     n_start = 0
     n_repeats = 100
-    n_jobs = -1
+    n_jobs = 24
 
     # Section: varying over sample-sizes
     n_samples_list = [2**x for x in range(8, 11)]
@@ -266,20 +266,20 @@ if __name__ == "__main__":
     )
 
     # Section: varying over dimensions
-    n_samples = 1024
-    n_dims_list = [2**i - 6 for i in range(3, 11)]
-    print(n_dims_list)
-    results = Parallel(n_jobs=n_jobs, verbose=True)(
-        delayed(_run_simulation)(
-            n_samples,
-            n_dims_1,
-            idx,
-            root_dir,
-            sim_name,
-            model_name,
-            overwrite=False,
-        )
-        for sim_name in SIMULATIONS_NAMES
-        for n_dims_1 in n_dims_list
-        for idx in range(n_start, n_repeats)
-    )
+    # n_samples = 1024
+    # n_dims_list = [2**i - 6 for i in range(3, 11)]
+    # print(n_dims_list)
+    # results = Parallel(n_jobs=n_jobs, verbose=True)(
+    #     delayed(_run_simulation)(
+    #         n_samples,
+    #         n_dims_1,
+    #         idx,
+    #         root_dir,
+    #         sim_name,
+    #         model_name,
+    #         overwrite=False,
+    #     )
+    #     for sim_name in SIMULATIONS_NAMES
+    #     for n_dims_1 in n_dims_list
+    #     for idx in range(n_start, n_repeats)
+    # )
