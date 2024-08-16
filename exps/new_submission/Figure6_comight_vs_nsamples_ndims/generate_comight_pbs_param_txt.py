57308260d18a36cd66cd27e53d65cc7a23be65f7
Adam Li <adam2392@gmail.com>
2024-08-16 11:20:14 -0400

    ADding new comight parameters
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- exps/new_submission/Figure6_comight_vs_nsamples_ndims/generate_comight_pbs_param_txt.py
+++ exps/new_submission/Figure6_comight_vs_nsamples_ndims/generate_comight_pbs_param_txt.py
@@ -12,10 +12,10 @@ with open(f"{curr_dir}/parameters_comight.txt", "w") as file:
     for sim_name in sim_names:
         for n_samples in [2**x for x in range(7, 11)]:
             for idx in range(n_repeats):
-                params = [idx, n_samples, 4096 - 6, sim_name]
+                params = [idx, n_samples, 4096 - 3, sim_name]
                 file.write(" ".join(map(str, params)) + "\n")
 
-        for n_dims_1 in [2**i - 6 for i in range(3, 13)]:
+        for n_dims_1 in [2**i - 3 for i in range(3, 13)]:
             for idx in range(n_repeats):
                 # seed, n_samples, n_dims_1, sim_name
                 params = [idx, 256, n_dims_1, sim_name]
